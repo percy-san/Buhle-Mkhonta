e6bda7cdef18c34ec60141646032b7b01b735500
Buhlebethu <percymkhonta@gmail.com>
2023-01-25 23:11:50 +0200

    fade animations

--- src/lib/components/Work.svelte
+++ src/lib/components/Work.svelte
@@ -11,22 +11,22 @@
 <Section heading ={"Work"} number={"03"} bottomText={"Some stuff I have done"}/>
 
 <div class="work-section">
-	<div class="project-1" uk-scrollspy="cls: uk-animation-slide-top; delay: 500; repeat: true">
+	<div class="project-1" uk-scrollspy="cls: uk-animation-fade; delay: 500; repeat: false">
 		<WorkCard projectName ={"Dreamcatcher Album preview"} image={"../images/dreamcatcher-album.jpeg"} link ={'https://album-page.vercel.app/'}/>
 		
 	</div>
 		
-	<div class="project-2" uk-scrollspy="cls: uk-animation-slide-bottom; delay: 600; repeat: true">
+	<div class="project-2" uk-scrollspy="cls: uk-animation-fade; delay: 600; repeat: false">
 		<WorkCard projectName ={"Tip Calulator"} image={"../images/tip-calculator.jpeg"} link={'https://tip-calculator-app-nu-self.vercel.app/'}/>
 			
 	</div>
 
-	<div class="project-3" uk-scrollspy="cls: uk-animation-slide-bottom; delay: 600; repeat: true">
+	<div class="project-3" uk-scrollspy="cls: uk-animation-fade; delay: 700; repeat: false">
 		<WorkCard projectName ={"Crowd Fund Challange"} image={"../images/crowdfunding.jpeg"} link={'https://crowdfund-challange.vercel.app/'}/>
 			
 	</div>
 
-	<div class="project-4" uk-scrollspy="cls: uk-animation-slide-bottom; delay: 600; repeat: true">
+	<div class="project-4" uk-scrollspy="cls: uk-animation-fade; delay: 800; repeat: false">
 		<WorkCard projectName ={"Blog"} image={"../images/Blog.jpeg"} link={'https://buhlebethu-mkhonta.netlify.app/'}/>
 			
 	</div>
